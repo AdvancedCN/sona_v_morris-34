b2c8ddb1a20cdab6023061cbb4fc025ec150bbf4
sonavmorris <93874702+sonavmorris@users.noreply.github.com>
2022-07-21 18:47:23 +0530

    Create prgm4.sh

---
+++ shell scripting/prgm4.sh
@@ -0,0 +1,16 @@
+4.How to set an array in Linux?
+
+1.IndirectDeclaration
+In Indirect declaration, We assigned a value in a particular index of 
+Array Variable.Noneed to first declare.
+ ARRAY NAME[INDEXNR]=value
+ 
+2.ExplicitDeclaration
+In Explicit Declaration, First We declare array then assigned the 
+values.
+ declare -a ARRAYNAME
+ 
+3.CompoundAssignment
+In Compount Assignment, We declare array with a bunch of 
+values. We can add other values later too.
+ ARRAYNAME=(value1 value2 .... valueN)
