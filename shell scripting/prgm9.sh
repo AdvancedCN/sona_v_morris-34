e4186aaccef953998c2be1f576aa8100930e9b19
sonavmorris <93874702+sonavmorris@users.noreply.github.com>
2022-07-21 18:53:01 +0530

    Create prgm9.sh

---
+++ shell scripting/prgm9.sh
@@ -0,0 +1,20 @@
+9.Write a shell script to check to see if the file “file_path” exists. If it does 
+exist, display “file_path passwords are enabled.” Next, check to see if you 
+can write to the file. If you can, display “You have permissions to edit 
+“file_path.””If you cannot, display “You do NOT have permissions to
+edit“file_path””?
+
+
+#!/bin/bash
+FILE=”/home/svimukthi/Assignment/sanka”
+if [ -e “$FILE” ]
+ then
+ echo “$FILE passwords are enabled”
+Networking And System Administration Lab
+49
+fi
+if [ -x “$FILE” ]
+ then
+ echo “You have permition to execute $FILE” else
+ echo “You do Not have permissions to execute $FILE”
+fi
