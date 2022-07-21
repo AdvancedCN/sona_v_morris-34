f7c3c8079fa1223983d0b1844fb5a29cfca834f7
sonavmorris <93874702+sonavmorris@users.noreply.github.com>
2022-07-21 18:52:17 +0530

    Create prgm8.sh

---
+++ shell scripting/prgm8.sh
@@ -0,0 +1,16 @@
+8.Write a script to compare numbers?
+
+ #!/bin/bash
+# Script to do numeric comparisons
+var1=10
+var2=20
+if [ $var2 -gt $var1 ]
+ then
+ echo "$var2 is greater than $var1"
+fi
+# Second comparison
+If [ $var1 -gt 30]then
+echo "$var is greater than 30"
+ else
+ echo "$var1 is less than 30"
+Fi
