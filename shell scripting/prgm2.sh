67848007a614f79f0c92cd61e4332acbfb55083a
sonavmorris <93874702+sonavmorris@users.noreply.github.com>
2022-07-21 18:44:08 +0530

    Create prgm2.sh

---
+++ shell scripting/prgm2.sh
@@ -0,0 +1,7 @@
+2. How to print the login names of all users on a system?
+
+ There are two options
+ Open the etc/passwd file by typing the command:
+cat etc/passwd
+Alternatively you can use the less command
+less etc/passwd
