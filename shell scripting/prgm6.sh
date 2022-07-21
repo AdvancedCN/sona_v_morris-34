7b3331d517e2c11ef7c658fd70914c1aaf700c28
sonavmorris <93874702+sonavmorris@users.noreply.github.com>
2022-07-21 18:49:50 +0530

    Create prgm6.sh

---
+++ shell scripting/prgm6.sh
@@ -0,0 +1,9 @@
+6.What is the difference between $* and $@?
+
+ $@ treats each quoted arguments as separate arguments but $* 
+considers 
+ the entire set of positional parameters as a single string.
+for var in “$@”
+ do
+echo “$var”
+Done
